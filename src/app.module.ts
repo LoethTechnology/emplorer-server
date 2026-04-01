f00d9bd31bbd52f5b9c0c48569ebefb5f4e8f953
Joseph Chikeme <jchikeme@gmail.com>
2026-04-02 01:29:15 +0300

    Update package dependencies and configure throttling and security middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { ReviewsModule } from './modules/reviews/reviews.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
     AuthModule,
     ReviewsModule,
   ],
