422b2981f7006f712d9832f8fdb6532632631152
Joseph Chikeme <jchikeme@gmail.com>
2026-05-17 14:11:46 +0300

    fix: change company_review findFirst to delete in UserService

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -258,7 +258,7 @@ export class UserService {
     await this.findActiveUserById(userId);
     await this.findOwnedReviewOrThrow(userId, reviewId);
 
-    await this.prismaService.company_review.findFirst({
+    await this.prismaService.company_review.delete({
       where: { id: reviewId },
     });
 
